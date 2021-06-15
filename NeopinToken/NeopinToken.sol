00b3754b40351c54650ad0b3f8402393f20c05fa
dev@neopin.io <dev@neopin.io>
2021-06-15 10:16:06 +0900

    Init ERC20 Neopin Token

---
+++ NeopinToken/NeopinToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract NPT is ERC20 {
+    constructor() ERC20("NEOPIN Token", "NPT") {
+        _mint(msg.sender, 1000000000e18);
+    }
+
+    function burn(uint256 _amount) public returns (bool) {
+        _burn(msg.sender, _amount);
+        return true;
+    }
+}
