65891df86dd9ef8d15f5cc76b421eef1d0e90a04
정설민 <soolmini@neowiz.com>
2021-06-22 18:18:06 +0900

    Token Contracts Edit Path

--- NeopinToken/NeopinToken.sol
+++ NeopinToken/NeopinToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NPT is ERC20 {
     constructor() ERC20("NEOPIN Token", "NPT") {
